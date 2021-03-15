45fa29f7bcd9525f2ffec03b68a39a6cc564088e
Ahmed Elshetany <samo7a98@mail.com>
2021-03-14 20:20:27 -0400

    file structure

---
+++ woof/src/scenes/LoginScene.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginScene = () => {
+    return (
+        <View>
+            <Text>
+                Login Page
+            </Text>
+        </View>
+    );
+}
+
+export default LoginScene; 
