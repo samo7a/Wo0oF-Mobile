9ce662f258997604adb4abc1ab8ced0afcc2efdb
Ahmed Elshetany <samo7a98@mail.com>
2021-03-24 00:28:31 -0400

    lets see again

--- woof/src/scenes/SignupScene.js
+++ woof/src/scenes/SignupScene.js
@@ -7,19 +7,23 @@ import { StyleSheet,
         Keyboard,
         SafeAreaView, 
         Alert, 
-        Modal, 
-        Button, 
+        Modal,  
         TouchableOpacity, 
         TextInput,
-        KeyboardAvoidingView
+        KeyboardAvoidingView,
+        ActivityIndicator
     } from 'react-native';
 import { useHistory} from "react-router-native";
 import LinearGradient from 'react-native-linear-gradient';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import logo from '../images/logo.png';
-import axios from 'axios';
-const storage = require('../TokenStorage');
+import defaultProfilePic from '../images/default-profile-with-dog.png';
+
+import * as ImagePicker from 'react-native-image-picker'; 
+
+import axios from '../utilities/axios';
+const storage = require('../utilities/TokenStorage');
 
 const SignupScene = () => {
     const history = useHistory();
@@ -29,15 +33,51 @@ const SignupScene = () => {
     const [password, setPassword] = useState('');
     const [isOwner, setIsOwner] = useState(false);
     const [visible, setVisible] = useState(false);
-    const [enableShift, setEnableShift] = useState(false);
+    const [profilePic, setProfilePic] = useState(defaultProfilePic);
+    const [address, setAddress] = useState('');
+    const [bio, setBio] = useState('');
+    
 
     const signupHandler = () => {
-       Alert.alert("Alert Title", `first name : ${firstName}, last name : ${lastName}, email : ${email}, password: F**k oIo, isOwner: ${isOwner}`);
        setVisible(false);
+       const json = {
+        Email: email,
+        Password: password, 
+        Location : address,
+        FirstName : firstName,
+        LastName : lastName,
+        isOwner : isOwner,
+        ProfilePicture : profilePic, 
+        ShortBio : bio
+       }
+       axios.post('/user', json)
+      .then(function (response) {
+        console.warn(response);
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
     }
     const showModal = () => {
         setVisible(true);
     }
+    const hideModal = () =>{
+        setVisible(false);
+    }
+    const choosePhoto = () => {
+        const options = {
+          noData: true,
+          mediaType: 'photo'
+        }
+        ImagePicker.launchImageLibrary(options, response => {
+          if (response.uri) {
+            setProfilePic(response);
+          }
+        })
+      }
+      const removePhoto = () => {
+          setProfilePic(defaultProfilePic);
+      }
     
     return (
         
@@ -118,25 +158,58 @@ const SignupScene = () => {
 
 
                         <Modal visible={visible} animationType='slide'>
-                        <LinearGradient colors={['#8D99AE','#EDF2F4']} style={{height:1300, flex : 1}}>
-                            <View style={styles.modal}>
-                            <Text style={styles.text}>Last Name</Text>
-                            <TextInput  style={styles.inputText} 
-                                        placeholder = "Last Name" 
-                                        onChangeText = {(e) => setLastName(e)} 
-                                        value ={lastName}
-                                        backgroundColor='white'
-                                        keyboardType = 'default'
-                            />
-                            <TouchableOpacity onPress={signupHandler}>
-                                <View style={styles.button}>
-                                <Text style={{ fontFamily: 'Arial', fontSize: 15, color: 'white', alignSelf: 'center' }}>
-                                    Signup
-                                <Icon name="chevron-up" size={15} color="white" />
-                                </Text>
+                            <LinearGradient colors={['#8D99AE','#EDF2F4']} style={{height:1300, flex : 1}}>
+                            <KeyboardAvoidingView behavior='height' style={{flex : 1,}} >
+                            <TouchableWithoutFeedback  onPress={Keyboard.dismiss} >
+                                <View style={styles.modal}>
+                                    <TouchableOpacity onPress={choosePhoto} style={{margin: 25}}>
+                                        <View style={{position:'relative', justifyContent: 'center', alignItems: 'center'}}>
+                                            <Image style={styles.photo} source= {profilePic}/>
+                                            <Text style={{color: 'white', position:'absolute', alignSelf:'center', textAlign: 'center'}}>Upload Profile Picture</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={removePhoto} style={styles.button}>
+                                        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                                            <Text style={{color: 'white', alignSelf:'center', textAlign: 'center', padding : 5}}>Remove Photo</Text>
+                        
+                                        </View>
+                                    </TouchableOpacity>
+                                    <Text style={styles.text}>Address</Text>
+                                    <TextInput  style={styles.inputText} 
+                                                placeholder = "Address" 
+                                                onChangeText = {(e) => setAddress(e)} 
+                                                value ={address}
+                                                backgroundColor='white'
+                                                keyboardType = 'default'
+                                    />
+                                    <Text style={styles.text}>Bio</Text>
+                                    <TextInput  style={styles.inputbio} 
+                                                placeholder = "Bio..." 
+                                                onChangeText = {(e) => setBio(e)} 
+                                                value ={bio}
+                                                backgroundColor='white'
+                                                keyboardType = 'default'
+                                                multiline = {true}
+                                    />
+                                    <TouchableOpacity onPress={signupHandler}>
+                                        <View style={styles.button}>
+                                        <Text style={{ fontFamily: 'Arial', fontSize: 15, color: 'white', alignSelf: 'center' }}>
+                                            Signup
+                                        <Icon name="chevron-up" size={15} color="white" />
+                                        </Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={hideModal} >
+                                        <View style={styles.button}>
+                                        <Text style={{ fontFamily: 'Arial', fontSize: 15, color: 'white', alignSelf: 'center' }}>
+                                            Cancel
+                                        <Icon name="remove" size={15} color="white" />
+                                        </Text>
+                                        </View>
+                                    </TouchableOpacity>
                                 </View>
-                            </TouchableOpacity>
-                            </View>
+                                </TouchableWithoutFeedback>
+                                </KeyboardAvoidingView>
                             </LinearGradient>
                         </Modal>
 
@@ -199,6 +272,21 @@ const styles = StyleSheet.create({
         width: '75%',
         margin: 10,
         fontWeight : '600',
+        alignSelf : 'center',
+    },
+    inputbio : {
+        borderWidth: 1,
+        borderColor: '#2B2D42',
+        borderStyle : 'solid',
+        padding : 10,
+        fontSize: 18,
+        borderRadius : 6,
+        height: 90,
+        width: '75%',
+        margin: 10,
+        fontWeight : '600',
+        alignSelf : 'center',
+        //textAlignVertical : 'top'
     },
     checkbox : {
         alignSelf : 'center',
@@ -223,8 +311,20 @@ const styles = StyleSheet.create({
         textDecorationLine: 'underline',
     },
     modal : {
-
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignContent: 'center',
+        padding : 10,
     },
+    photo : {
+        height: 150,
+        width : 150,
+        borderRadius : 75,
+        alignSelf : 'center',
+        justifyContent : 'space-evenly',
+
+        
+    }
 });
 
 export default SignupScene; 
